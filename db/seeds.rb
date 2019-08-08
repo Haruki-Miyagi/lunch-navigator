687f101a73bd12eef71a7b420e697942ff261aa7
Moriyuki Arakawa <arakawamoriyuki@gmail.com>
2019-08-09 01:44:30 +0900

    ダミーデータとしてseed用意した

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+test1_user = User.create(account: 'test1@sample.com')
+test2_user = User.create(account: 'test2@sample.com')
+
+Star.create(
+    user_id: test1_user.id,
+    gnavi_shop_id: 'fbgm600',
+)
+Star.create(
+    user_id: test2_user.id,
+    gnavi_shop_id: 'f588302',
+)
+
+Review.create(
+    user_id: test1_user.id,
+    gnavi_shop_id: 'fbgm600',
+    score: 3,
+    comment: 'あんまり美味しくなかった',
+)
+
+Review.create(
+    user_id: test2_user.id,
+    gnavi_shop_id: 'fbdc600',
+    score: 5,
+    comment: 'とても美味しかった',
+)
