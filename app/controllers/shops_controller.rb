20403bd063d230040544bed5ead3eed7382d9c72
Moriyuki Arakawa <arakawamoriyuki@gmail.com>
2019-08-09 01:44:15 +0900

    GurunabiをラップしたLunchmap.fetchを作った。dbに保存してるreviewとかstarデータも取れるようになる

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,6 +1,6 @@
 class ShopsController < ApplicationController
   def index
-    response = Gurunabi.fetch_gurunabi_api(
+    response = Lunchmap.fetch(
       private_room: 1,
       card: 1,
       lunch: 1,
@@ -8,6 +8,10 @@ class ShopsController < ApplicationController
       longitude: 127.679021,
       # 緯度/経度からの検索範囲(半径) 1:300m,2:500m,3:1000m,4:2000m,5=3000m
       range: 5,
+
+      # latitude: 26.432278099999998,
+      # longitude: 127.75907720000001,
+      # range: 1,
     )
 
     @resouces = response['rest']
