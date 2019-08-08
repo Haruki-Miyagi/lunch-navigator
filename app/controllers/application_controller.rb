a9bf67671c2738657272aac59ff93d66a51010a1
haruki <haruki.miyagi1112@gmail.com>
2019-08-08 22:18:03 +0900

    Add auth0

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+  # before_action :authenticate_user!
+
+  def signed_user?
+    session[:userinfo].present?
+  end
+
+  def authenticate_user!
+    if signed_user?
+      @current_user ||= session[:userinfo]
+    else
+      @auth_path = "#{controller_path}##{action_name}"
+      redirect_to authentication_path if @auth_path != 'auth0#callback'
+    end
+  end
+
+  def reset_session
+    session[:userinfo] = nil if signed_user?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :lunchmaps
+
+  root 'lunchmaps#index'
+
+  get 'dashboard' => 'home#dashboard'
+  get 'auth/auth0', as: 'authentication'
+  get 'auth/auth0/callback' => 'auth0#callback'
+  get 'logout' => 'auth0#logout'
 end
