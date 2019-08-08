866ea89a5a5d58f2eb6f1ced8d01c6b16b038b68
haruki <haruki.miyagi1112@gmail.com>
2019-08-08 22:29:58 +0900

    destroy path lunchmaps/index

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :lunchmaps
 
-  root 'lunchmaps#index'
-
   get 'dashboard' => 'home#dashboard'
   get 'auth/auth0', as: 'authentication'
   get 'auth/auth0/callback' => 'auth0#callback'
