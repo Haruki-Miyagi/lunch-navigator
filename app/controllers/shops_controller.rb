4f6a9571131a00afe251f1d8750b578a37fcc217
Moriyuki Arakawa <arakawamoriyuki@gmail.com>
2019-08-09 00:57:31 +0900

    APIリファレンスに乗ってるパラメータを自由にリクエストできるようにした。緯度経度とその範囲をパラメータに含めた

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,8 +1,15 @@
 class ShopsController < ApplicationController
-  
   def index
-    @resouces = Gurunabi.fetch_gurunabi_api(1, 1, 1)
-    @resouces = @resouces['rest']
+    response = Gurunabi.fetch_gurunabi_api(
+      private_room: 1,
+      card: 1,
+      lunch: 1,
+      latitude: 26.212576,
+      longitude: 127.679021,
+      # 緯度/経度からの検索範囲(半径) 1:300m,2:500m,3:1000m,4:2000m,5=3000m
+      range: 5,
+    )
+
+    @resouces = response['rest']
   end
 end
-    
