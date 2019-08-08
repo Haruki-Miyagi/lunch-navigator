c05d4270ef3608d5ae77594d6812d0f1e1d353f8
haruki <haruki.miyagi1112@gmail.com>
2019-08-08 18:26:39 +0900

    Update fetch_gurunabi_api

--- lib/gurunabi.rb
+++ lib/gurunabi.rb
@@ -4,27 +4,7 @@ class Gurunabi
     gurunabi_api = Rails.application.config.gurunabi_url + "&private_room=#{private_room}&card=#{card}&lunch=#{lunch}"
     uri_encode = URI.encode(gurunabi_api)
     uri = URI.parse(uri_encode)
-    begin
-      response = Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme == 'https') do |http|
-        http.open_timeout = 5
-        http.read_timeout = 10
-        http.get(uri.request_uri)
-      end
-
-      case response
-        when Net::HTTPSuccess
-          json = JSON.parse(response.body)
-          if json['results_returned'] == 0
-            nil
-          else
-            json
-          end
-        else
-          Rails.logger.debug(uri.to_s)
-      end
-
-    rescue => e
-      Rails.logger.debug(e.message)
-    end
+    json = Net::HTTP.get(uri)
+    JSON.parse(json)
   end
 end
