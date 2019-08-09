e8f7286a23747bf2ae0711066249960aec017904
haruki <haruki.miyagi1112@gmail.com>
2019-08-09 13:54:16 +0900

    Fix auth star

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,11 @@
 module ApplicationHelper
+  def current_user
+    user = session[:userinfo]
+    if user.present?
+      @current_user ||= User.where(account: user['uid'])
+    end
+  end
+
   def current_user_name
     @user_name ||=  session[:userinfo]['info']['name'].split.first
   end
