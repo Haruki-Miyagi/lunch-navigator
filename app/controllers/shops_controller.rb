601ff80e0a6eb63a27eea644ea7192c7c880b97c
Moriyuki Arakawa <arakawamoriyuki@gmail.com>
2019-08-09 14:34:58 +0900

    固定値ではなくパラメータを利用する

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -4,8 +4,8 @@ class ShopsController < ApplicationController
       private_room: params[:private_room],
       card: params[:card],
       lunch: params[:lunch],
-      latitude: 26.212576,
-      longitude: 127.679021,
+      latitude: params[:latitude],
+      longitude: params[:longitude],
       # 緯度/経度からの検索範囲(半径) 1:300m,2:500m,3:1000m,4:2000m,5=3000m
       range: 5,
     )
